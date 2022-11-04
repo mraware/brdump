c86c34c26688477db4eca6de565e214361d9f9e8
mraware <aaronware14@gmail.com>
2022-11-04 14:15:50 -0500

    only include properties that can network

--- omegga.main.js
+++ omegga.main.js
@@ -25,7 +25,7 @@ class Dump {
   dumpAll = async () => {
     try {
       const classRegExp = /^(?<index>\d+)\) (?<type>.*) (?<className>.*)$/;
-      const propsRegExp = /^(?<index>\d+)\) (?<prop>.*)$/;
+      const propsRegExp = /^(.*Prop (?<prop>.+) at offset .*type (?<type>.+))|(.*Flag (?<flag>CPF_.+))$/;
 
       const dumpPath = path.join(__dirname, 'dump');
       if (fs.existsSync(dumpPath)) {
@@ -34,7 +34,7 @@ class Dump {
       fs.mkdirSync(dumpPath);
 
 
-      const classesMatch = await this.omegga.watchLogChunk('GetAll Class', classRegExp, {first: 'index'});
+      const classesMatch = await this.omegga.watchLogChunk('GetAll Class', classRegExp, { first: 'index' });
 
       const files = [];
       const weapons = [];
@@ -47,12 +47,29 @@ class Dump {
             const weaponName = splitName[splitName.length - 1].split('.')[0]
             if (weaponName.startsWith('Weapon_')) weapons.push(weaponName);
           }
-          console.log(`Read: ${i+1}/${classesMatch.length}: ${className}`);
-          const propsMatch = await this.omegga.watchLogChunk(`ListProps ${className} *`, propsRegExp, {first: 'index'});
+          console.log(`Read: ${i + 1}/${classesMatch.length}: ${className}`);
+          const propsMatch = await this.omegga.watchLogChunk(`ListProps ${className} *`, propsRegExp, {});
           if (propsMatch) {
-            const data = propsMatch.reduce((prev, curr) => prev+curr.groups.prop+'\n', `${className}\n\n`);
-            const folderPath = path.join(dumpPath, type, className.slice(0, className.lastIndexOf('/')));
-            files.push({ data, folderPath, type, className });
+            const allProps = []
+            let currProp = {}
+            for (let { groups: props } of propsMatch) {
+              if (props) {
+                const { prop, type, flag } = props;
+                if (prop) {
+                  currProp = { prop, type };
+                } else if (flag && currProp) {
+                  if (flag === 'CPF_Net') {
+                    allProps.push(currProp)
+                    currProp = null;
+                  }
+                }
+              }
+            }
+            if (allProps.length > 0) {
+              const data = allProps.reduce((prev, curr) => prev + `${curr.prop} (${curr.type})` + '\n', `${className}\n\n`);
+              const folderPath = path.join(dumpPath, type, className.slice(0, className.lastIndexOf('/')));
+              files.push({ data, folderPath, type, className });
+            }
           } else {
             console.error(`Failed to get props for ${className}`);
           }
@@ -61,8 +78,8 @@ class Dump {
 
         fs.writeFileSync(path.join(dumpPath, 'Weapons'), JSON.stringify(weapons, null, 2));
 
-        files.forEach(({data, folderPath, type, className}, index) => {
-          console.log(`Write: ${index+1}/${classesMatch.length}: ${className}`);
+        files.forEach(({ data, folderPath, type, className }, index) => {
+          console.log(`Write: ${index + 1}/${files.length}: ${className}`);
           if (!fs.existsSync(folderPath)) {
             fs.mkdirSync(folderPath, { recursive: true });
           }
@@ -70,7 +87,7 @@ class Dump {
         });
       }
 
-      const packageMatch = await this.omegga.watchLogChunk('GetAll Package', classRegExp, {first: 'index'});
+      const packageMatch = await this.omegga.watchLogChunk('GetAll Package', classRegExp, { first: 'index' });
 
       if (packageMatch.length) {
         const bricks = [];
@@ -85,7 +102,7 @@ class Dump {
 
         fs.writeFileSync(path.join(dumpPath, 'Bricks'), JSON.stringify(bricks, null, 2));
       }
-    } catch(error) {
+    } catch (error) {
       console.error(error);
     }
   }
